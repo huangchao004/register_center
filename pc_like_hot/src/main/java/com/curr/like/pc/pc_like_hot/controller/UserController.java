e39e0ff2564d83324b3698a5f16b2e1270dfd388
huangchao <892248990@qq.com>
2018-01-18 09:46:13 +0800

    添加登录服务

--- pc_like_hot/src/main/java/com/curr/like/pc/pc_like_hot/controller/UserController.java
+++ pc_like_hot/src/main/java/com/curr/like/pc/pc_like_hot/controller/UserController.java
@@ -1,32 +1,34 @@
-package com.curr.like.pc.pc_like_hot;
+package com.curr.like.pc.pc_like_hot.controller;
 
 import com.curr.like.pc.pc_like_hot.feign.IPcUserService;
 import com.curry.like.model.model.response.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-public class TestController {
-
+public class UserController {
     @Autowired
     private IPcUserService iPcUserService;
-    private Logger logger = LoggerFactory.getLogger(TestController.class);
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseBody userServiceLogin(@RequestBody Map<String, Object> params) {
-        logger.info("userServiceLogin "+params.toString());
+        logger.info("userServiceLogin " + params.toString());
         return iPcUserService.login(params);
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseBody userServiceRegister(@RequestBody Map<String, Object> params) {
-        logger.info("userServiceRegister "+params.toString());
+        logger.info("userServiceRegister " + params.toString());
         return iPcUserService.register(params);
     }
 }
